5712a047be1a2014fc28692f21c73f3590339024
frankigogo19-beep <frankigogo19@gmail.com>
2026-09-18 23:06:58 +0300

    Create Companies component for managing companies

---
+++ companies/page.js
@@ -0,0 +1,10 @@
+export default function Companies() {
+  return (
+    <main>
+      <h1>Companies</h1>
+      <p>Manage companies and importers.</p>
+
+      <button>Add Company</button>
+    </main>
+  );
+}
